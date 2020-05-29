b4f7831b6964cdcbd92ad2757661814f9377bed0
oscars8a <oscars.soto.ochoa@gmail.com>
2020-05-30 00:59:46 +0200

    [ADD] config file

--- odoo_install.sh
+++ odoo_install.sh
@@ -167,115 +167,116 @@ do
   sudo su $OE_USER -c "git clone -b $OE_VERSION $line $OE_HOME/custom/$dir_name"
 done < $input
 
-# #--------------------------------------------------
-# ### Create server config file
-# #--------------------------------------------------
-# echo -e "* Create server config file"
-# sudo touch /etc/${OE_CONFIG}.conf
-# sudo su root -c "printf '[options] \n; This is the password that allows database operations:\n' >> /etc/${OE_CONFIG}.conf"
-# if [ $GENERATE_RANDOM_PASSWORD = "True" ]; then
-#     echo -e "* Generating random admin password"
-#     OE_SUPERADMIN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
-# fi
-# sudo su root -c "printf 'admin_passwd = ${OE_SUPERADMIN}\n' >> /etc/${OE_CONFIG}.conf"
-# if [ $OE_VERSION > "11.0" ];then
-#     sudo su root -c "printf 'http_port = ${OE_PORT}\n' >> /etc/${OE_CONFIG}.conf"
-# else
-#     sudo su root -c "printf 'xmlrpc_port = ${OE_PORT}\n' >> /etc/${OE_CONFIG}.conf"
-# fi
-# sudo su root -c "printf 'logfile = /var/log/${OE_USER}/${OE_CONFIG}.log\n' >> /etc/${OE_CONFIG}.conf"
+#--------------------------------------------------
+### Create server config file
+#--------------------------------------------------
+echo -e "* Create server config file"
+sudo cp oe_config.conf /etc/${OE_CONFIG}.conf
+sudo su root -c "printf '[options] \n; This is the password that allows database operations:\n' >> /etc/${OE_CONFIG}.conf"
+if [ $GENERATE_RANDOM_PASSWORD = "True" ]; then
+    echo -e "* Generating random admin password"
+    OE_SUPERADMIN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+fi
+sudo su root -c "printf 'admin_passwd = ${OE_SUPERADMIN}\n' >> /etc/${OE_CONFIG}.conf"
+if [ $OE_VERSION > "11.0" ];then
+    sudo su root -c "printf 'http_port = ${OE_PORT}\n' >> /etc/${OE_CONFIG}.conf"
+else
+    sudo su root -c "printf 'xmlrpc_port = ${OE_PORT}\n' >> /etc/${OE_CONFIG}.conf"
+fi
+sudo su root -c "printf 'logfile = /var/log/${OE_USER}/${OE_CONFIG}.log\n' >> /etc/${OE_CONFIG}.conf"
 
-# if [ $IS_ENTERPRISE = "True" ]; then
-#     sudo su root -c "printf 'addons_path=${OE_HOME}/enterprise/addons,${OE_HOME_EXT}/addons\n' >> /etc/${OE_CONFIG}.conf"
-# else
-#     sudo su root -c "printf 'addons_path=${OE_HOME_EXT}/addons,${OE_HOME}/custom/addons\n' >> /etc/${OE_CONFIG}.conf"
-# fi
-# sudo chown $OE_USER:$OE_USER /etc/${OE_CONFIG}.conf
-# sudo chmod 640 /etc/${OE_CONFIG}.conf
+if [ $IS_ENTERPRISE = "True" ]; then
+    sudo su root -c "printf 'addons_path=${OE_HOME}/enterprise/addons,${OE_HOME_EXT}/addons\n' >> /etc/${OE_CONFIG}.conf"
+else
+    sudo su root -c "printf 'addons_path=${OE_HOME_EXT}/addons,${OE_HOME}/custom/addons\n' >> /etc/${OE_CONFIG}.conf"
+fi
 
+sudo chown $OE_USER:$OE_USER /etc/${OE_CONFIG}.conf
+sudo chmod 640 /etc/${OE_CONFIG}.conf
+--------------------------------------------------
 # echo -e "* Create startup file"
 # sudo su root -c "echo '#!/bin/sh' >> $OE_HOME_EXT/start.sh"
 # sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/odoo-bin --config=/etc/${OE_CONFIG}.conf' >> $OE_HOME_EXT/start.sh"
 # sudo chmod 755 $OE_HOME_EXT/start.sh
 
-# #--------------------------------------------------
-# # Adding ODOO as a deamon (initscript)
-# #--------------------------------------------------
+#--------------------------------------------------
+# Adding ODOO as a deamon (initscript)
+#--------------------------------------------------
 
-# echo -e "* Create init file"
-# cat <<EOF > ~/$OE_CONFIG
-# #!/bin/sh
-# ### BEGIN INIT INFO
-# # Provides: $OE_CONFIG
-# # Required-Start: \$remote_fs \$syslog
-# # Required-Stop: \$remote_fs \$syslog
-# # Should-Start: \$network
-# # Should-Stop: \$network
-# # Default-Start: 2 3 4 5
-# # Default-Stop: 0 1 6
-# # Short-Description: Enterprise Business Applications
-# # Description: ODOO Business Applications
-# ### END INIT INFO
-# PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
-# DAEMON=$OE_HOME_EXT/odoo-bin
-# NAME=$OE_CONFIG
-# DESC=$OE_CONFIG
-# # Specify the user name (Default: odoo).
-# USER=$OE_USER
-# # Specify an alternate config file (Default: /etc/openerp-server.conf).
-# CONFIGFILE="/etc/${OE_CONFIG}.conf"
-# # pidfile
-# PIDFILE=/var/run/\${NAME}.pid
-# # Additional options that are passed to the Daemon.
-# DAEMON_OPTS="-c \$CONFIGFILE"
-# [ -x \$DAEMON ] || exit 0
-# [ -f \$CONFIGFILE ] || exit 0
-# checkpid() {
-# [ -f \$PIDFILE ] || return 1
-# pid=\`cat \$PIDFILE\`
-# [ -d /proc/\$pid ] && return 0
-# return 1
-# }
-# case "\${1}" in
-# start)
-# echo -n "Starting \${DESC}: "
-# start-stop-daemon --start --quiet --pidfile \$PIDFILE \
-# --chuid \$USER --background --make-pidfile \
-# --exec \$DAEMON -- \$DAEMON_OPTS
-# echo "\${NAME}."
-# ;;
-# stop)
-# echo -n "Stopping \${DESC}: "
-# start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
-# --oknodo
-# echo "\${NAME}."
-# ;;
-# restart|force-reload)
-# echo -n "Restarting \${DESC}: "
-# start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
-# --oknodo
-# sleep 1
-# start-stop-daemon --start --quiet --pidfile \$PIDFILE \
-# --chuid \$USER --background --make-pidfile \
-# --exec \$DAEMON -- \$DAEMON_OPTS
-# echo "\${NAME}."
-# ;;
-# *)
-# N=/etc/init.d/\$NAME
-# echo "Usage: \$NAME {start|stop|restart|force-reload}" >&2
-# exit 1
-# ;;
-# esac
-# exit 0
-# EOF
+echo -e "* Create init file"
+cat <<EOF > ~/$OE_CONFIG
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides: $OE_CONFIG
+# Required-Start: \$remote_fs \$syslog
+# Required-Stop: \$remote_fs \$syslog
+# Should-Start: \$network
+# Should-Stop: \$network
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Enterprise Business Applications
+# Description: ODOO Business Applications
+### END INIT INFO
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+DAEMON=$OE_HOME_EXT/odoo-bin
+NAME=$OE_CONFIG
+DESC=$OE_CONFIG
+# Specify the user name (Default: odoo).
+USER=$OE_USER
+# Specify an alternate config file (Default: /etc/openerp-server.conf).
+CONFIGFILE="/etc/${OE_CONFIG}.conf"
+# pidfile
+PIDFILE=/var/run/\${NAME}.pid
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS="-c \$CONFIGFILE"
+[ -x \$DAEMON ] || exit 0
+[ -f \$CONFIGFILE ] || exit 0
+checkpid() {
+[ -f \$PIDFILE ] || return 1
+pid=\`cat \$PIDFILE\`
+[ -d /proc/\$pid ] && return 0
+return 1
+}
+case "\${1}" in
+start)
+echo -n "Starting \${DESC}: "
+start-stop-daemon --start --quiet --pidfile \$PIDFILE \
+--chuid \$USER --background --make-pidfile \
+--exec \$DAEMON -- \$DAEMON_OPTS
+echo "\${NAME}."
+;;
+stop)
+echo -n "Stopping \${DESC}: "
+start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
+--oknodo
+echo "\${NAME}."
+;;
+restart|force-reload)
+echo -n "Restarting \${DESC}: "
+start-stop-daemon --stop --quiet --pidfile \$PIDFILE \
+--oknodo
+sleep 1
+start-stop-daemon --start --quiet --pidfile \$PIDFILE \
+--chuid \$USER --background --make-pidfile \
+--exec \$DAEMON -- \$DAEMON_OPTS
+echo "\${NAME}."
+;;
+*)
+N=/etc/init.d/\$NAME
+echo "Usage: \$NAME {start|stop|restart|force-reload}" >&2
+exit 1
+;;
+esac
+exit 0
+EOF
 
-# echo -e "* Security Init File"
-# sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
-# sudo chmod 755 /etc/init.d/$OE_CONFIG
-# sudo chown root: /etc/init.d/$OE_CONFIG
+echo -e "* Security Init File"
+sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
+sudo chmod 755 /etc/init.d/$OE_CONFIG
+sudo chown root: /etc/init.d/$OE_CONFIG
 
-# echo -e "* Start ODOO on Startup"
-# sudo update-rc.d $OE_CONFIG defaults
+echo -e "* Start ODOO on Startup"
+sudo update-rc.d $OE_CONFIG defaults
 
 # #--------------------------------------------------
 # # Install Nginx if needed
@@ -380,20 +381,20 @@ done < $input
 #   echo "SSL/HTTPS isn't enabled due to choice of the user or because of a misconfiguration!"
 # fi
 
-# echo -e "* Starting Odoo Service"
-# sudo su root -c "/etc/init.d/$OE_CONFIG start"
-# echo "-----------------------------------------------------------"
-# echo "Done! The Odoo server is up and running. Specifications:"
-# echo "Port: $OE_PORT"
-# echo "User service: $OE_USER"
-# echo "User PostgreSQL: $OE_USER"
-# echo "Code location: $OE_USER"
-# echo "Addons folder: $OE_USER/$OE_CONFIG/addons/"
-# echo "Password superadmin (database): $OE_SUPERADMIN"
-# echo "Start Odoo service: sudo service $OE_CONFIG start"
-# echo "Stop Odoo service: sudo service $OE_CONFIG stop"
-# echo "Restart Odoo service: sudo service $OE_CONFIG restart"
-# if [ $INSTALL_NGINX = "True" ]; then
-#   echo "Nginx configuration file: /etc/nginx/sites-available/odoo"
-# fi
-# echo "-----------------------------------------------------------"
+echo -e "* Starting Odoo Service"
+sudo su root -c "/etc/init.d/$OE_CONFIG start"
+echo "-----------------------------------------------------------"
+echo "Done! The Odoo server is up and running. Specifications:"
+echo "Port: $OE_PORT"
+echo "User service: $OE_USER"
+echo "User PostgreSQL: $OE_USER"
+echo "Code location: $OE_USER"
+echo "Addons folder: $OE_USER/$OE_CONFIG/addons/"
+echo "Password superadmin (database): $OE_SUPERADMIN"
+echo "Start Odoo service: sudo service $OE_CONFIG start"
+echo "Stop Odoo service: sudo service $OE_CONFIG stop"
+echo "Restart Odoo service: sudo service $OE_CONFIG restart"
+if [ $INSTALL_NGINX = "True" ]; then
+  echo "Nginx configuration file: /etc/nginx/sites-available/odoo"
+fi
+echo "-----------------------------------------------------------"
